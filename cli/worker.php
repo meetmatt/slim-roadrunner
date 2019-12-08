60df74993e2461393233f9c38802c4586bb8c753
Yuri Golikov <meetmatt@ya.ru>
2019-12-08 14:55:28 +0100

    Added Slim 4

--- cli/worker.php
+++ cli/worker.php
@@ -1,24 +1,41 @@
 <?php declare(strict_types=1);
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Factory\AppFactory;
 use Spiral\Goridge\StreamRelay;
 use Spiral\RoadRunner\Worker;
 use Spiral\RoadRunner\PSR7Client;
 use Nyholm\Psr7\Factory\Psr17Factory;
 
 ini_set('display_errors', 'stderr');
+error_reporting(E_ALL);
+chdir(__DIR__);
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
 include __DIR__ . '/../vendor/autoload.php';
+// $container = require __DIR__ . '/../config/container.php';
 
 $relay   = new StreamRelay(STDIN, STDOUT);
 $worker  = new Worker($relay);
 $factory = new Psr17Factory();
 $client  = new PSR7Client($worker, $factory, $factory, $factory);
 
-while ($req = $client->acceptRequest()) {
+$application = AppFactory::create();
+$application->get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
+    $response->getBody()->write('Slim on Road Runner');
+
+    return $response;
+});
+
+while ($request = $client->acceptRequest()) {
     try {
-        $response = $factory->createResponse();
-        $response->getBody()->write('Road runner');
+        $response = $application->handle($request);
         $client->respond($response);
     } catch (Throwable $e) {
         $client->getWorker()->error((string)$e);
     }
+    gc_collect_cycles();
 }
